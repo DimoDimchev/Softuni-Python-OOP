dc9a75fbce672094e9b6a3d5dbc988be0f1b7cb9
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-04 23:25:14 +0200

    Add files via upload

---
+++ Encapsulation/Email Validator.py
@@ -0,0 +1,32 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __validate_name(self, name):
+        return len(name) >= self.min_length
+
+    def __validate_mail(self, mail):
+        return mail in self.mails
+
+    def __validate_domain(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        curr_list = email.split("@")
+        name = curr_list[0]
+        mail = (curr_list[1].split("."))[0]
+        domain = (curr_list[1].split("."))[1]
+        if self.__validate_name(name) and self.__validate_mail(mail) and self.__validate_domain(domain):
+            return True
+        return False
+
+
+# mails = ["gmail", "softuni"]
+# domains = ["com", "bg"]
+# email_validator = EmailValidator(6, mails, domains)
+# print(email_validator.validate("pe77er@gmail.com"))
+# print(email_validator.validate("georgios@gmail.net"))
+# print(email_validator.validate("stamatito@abv.net"))
+# print(email_validator.validate("abv@softuni.bg"))
