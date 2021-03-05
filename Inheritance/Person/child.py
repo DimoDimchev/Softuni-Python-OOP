e388bee9a73c7286dbe87c7d984ca385534e61d3
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-05 23:30:21 +0200

    Add files via upload

---
+++ Inheritance/Person/child.py
@@ -0,0 +1,12 @@
+from project.person import Person
+
+
+class Child(Person):
+    def __init__(self, name, age):
+        Person.__init__(self, name, age)
+
+# person = Person("Peter", 25)
+# child = Child("Peter Junior", 5)
+# print(person.name)
+# print(person.age)
+# print(child.__class__.__bases__[0].__name__)
