d2ca7beccdb75fc6b2a8079bc146ab905acf914d
Dimo Dimchev <ddimchev7@gmail.com>
2021-03-05 23:26:44 +0200

    Add files via upload

---
+++ Inheritance/stack_of_strings.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, value):
+        return self.data.append(value)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[0]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return f"[{', '.join(reversed(self.data))}]"
